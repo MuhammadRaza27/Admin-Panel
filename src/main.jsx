aaffbef5fc860228cc72ab3ea1f9843e4e926bee
Muhammad Raza <szkhan2526@gmail.com>
2025-08-10 03:41:30 +0500

    Redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@coreui/coreui/dist/css/coreui.min.css'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+    <HashRouter>
     <App />
+    </HashRouter>
     </BrowserRouter>
   </StrictMode>,
 )
