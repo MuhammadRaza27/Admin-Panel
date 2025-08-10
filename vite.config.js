75a3dbdb00918751bbe69e9894551af76cc303fc
Muhammad Raza <szkhan2526@gmail.com>
2025-08-10 06:21:45 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MuhammadRaza27/Admin-Panel/', // <-- EXACT repo name
+  base: '/Admin-Panel', // <-- EXACT repo name
 })
